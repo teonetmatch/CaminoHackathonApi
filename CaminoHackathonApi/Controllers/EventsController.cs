a103b55c9414854201c75ebc6fb0e311d35b917e
Teodora Naste <t.naste@netmatch.ro>
2025-05-08 09:47:30 +0300

    Create a POST endpoint that receives an event

---
+++ CaminoHackathonApi/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using CaminoHackathonApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaminoHackathonApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class EventsController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult ReceiveEvent([FromBody] Event eventReceived)
+		{
+			var message = $"Event received: {eventReceived.EventType} - {eventReceived.Name} at {eventReceived.StartsAt}";
+			return Ok(message);
+		}
+	}
+}
---
+++ CaminoHackathonApi/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace CaminoHackathonApi.Models
+{
+	public class Event
+	{
+		public int Id { get; set; }
+		public string EventType { get; set; }
+		public string Name { get; set; }
+		public DateTime StartsAt { get; set; }
+		public DateTime EndsAt { get; set; }
+		public double Longitude { get; set; }
+		public double Latitude { get; set; }
+	}
+}
