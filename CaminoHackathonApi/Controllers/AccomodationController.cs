7e9fe082cecb90aa8bc2a7c0a681a6d6bcd3a7aa
Teodora Naste <t.naste@netmatch.ro>
2025-05-08 16:26:25 +0300

    Search after events were received

--- CaminoHackathonApi/Controllers/AccomodationController.cs
+++ CaminoHackathonApi/Controllers/AccomodationController.cs
@@ -1,26 +1,25 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using CaminoHackathonApi.Models;
+﻿using CaminoHackathonApi.Models;
 using CaminoHackathonApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CaminoHackathonApi.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AccommodationController : ControllerBase
-    {
-        private readonly IAccommodationSearchService _searchSvc;
+	[ApiController]
+	[Route("api/[controller]")]
+	public class AccommodationController : ControllerBase
+	{
+		private readonly IAccommodationSearchService _searchSvc;
 
-        public AccommodationController(IAccommodationSearchService searchSvc)
-        {
-            _searchSvc = searchSvc;
-        }
+		public AccommodationController(IAccommodationSearchService searchSvc)
+		{
+			_searchSvc = searchSvc;
+		}
 
-        [HttpPost("search")]
-        public async Task<IActionResult> Search([FromBody] AccommodationSearchRequestDto dto)
-        {
-            var grpcResp = await _searchSvc.SearchAsync(dto);
-            return Ok(grpcResp);
-        }
-    }
+		[HttpPost("search")]
+		public async Task<IActionResult> Search([FromBody] AccommodationSearchRequestDto dto)
+		{
+			var grpcResp = await _searchSvc.SearchAsync(dto);
+			return Ok(grpcResp);
+		}
+	}
 }
--- CaminoHackathonApi/Controllers/EventsController.cs
+++ CaminoHackathonApi/Controllers/EventsController.cs
@@ -1,19 +1,38 @@
-﻿using System.Diagnostics;
-using CaminoHackathonApi.Models;
+﻿using CaminoHackathonApi.Models;
+using CaminoHackathonApi.Services;
+using Cmp.Services.Accommodation.V1;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CaminoHackathonApi.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class EventsController : ControllerBase
+	public class EventsController(IAccommodationSearchService searchService) : ControllerBase
 	{
+		private readonly IAccommodationSearchService _searchSvc = searchService;
+
 		[HttpPost]
-		public IActionResult ReceiveEvent([FromBody] Event eventReceived)
+		public async Task<ActionResult<AccommodationSearchResponse>> ReceiveEventAsync([FromBody] Event eventReceived)
 		{
 			var message = $"Event received: {eventReceived.EventType} - {eventReceived.Name} at {eventReceived.StartsAt}";
 			Console.WriteLine(message);
-			return Ok(message);
+
+			_ = Task.Run(async() =>
+			{
+				Console.WriteLine("Searching for hotels...");
+
+				var result = await _searchSvc.SearchAsync(new AccommodationSearchRequestDto
+				{
+					StartDate = eventReceived.StartsAt,
+					EndDate = eventReceived.EndsAt,
+					Latitude = eventReceived.Latitude,
+					Longitude = eventReceived.Longitude
+				});
+
+				Console.WriteLine($"Found {result.Results.Count}");
+			});
+
+			return Ok();
 		}
 	}
 }
